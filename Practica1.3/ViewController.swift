70b6bfa64962b14b8aaa1a4d20a801d188ddda77
Alumno <ulsa@D105.local>
2022-10-21 08:35:17 -0700

    1era

--- Practica1.3/ViewController.swift
+++ Practica1.3/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swpermiso: UISwitch!
+    @IBOutlet weak var lblTexto: UILabel!
+    
+    @IBOutlet weak var lblAdvertencia: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func doTapModificar(_ sender: Any) {
+        lblAdvertencia.isHidden = true
+        if swpermiso.isOn
+        {
+            performSegue(withIdentifier: "goToModificar", sender: self)
+        }else
+        {
+            lblAdvertencia.isHidden = false
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destino = segue.destination as! ModificarController
+        destino.texto = lblTexto.text
+        destino.callbackActualizar = actualizarEtiqueta
+        
+    }
+    
+    func actualizarEtiqueta(texto:String){
+        lblTexto.text = texto
+    }
+    
+    
+    
 }
+    
+
+
+
+
+
+
+
+
 
